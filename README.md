d7b947a308adcb7a83d452a3cb25462c0fb7c85a
Czuz <czuz.pl@icloud.com>
2021-09-18 15:47:11 +0200

    Dostosowanie skryptu do aktualnej struktury strony xyz.naszemiasto.pl Dodanie do nagłówka User-Agent - bez tego strona blokuje zarówno działanie skryptu, jak i WGETa.

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ Sekwencyjne, aczkolwiek sprawne i 10 razy szybsze od przeklikiwania tamtej pasku
 Usuńcie f-stringi dla zmniejszenia wymaganej wersji Pythona  
 
 ### Wymagania:
-**requests, bs4**
+**requests, bs4, re**
 
 ## Zastosowanie:
 
 ```bash
 python3 naszemiasto.py <URL> # URL do pierwszego zdjęcia w otwartej galerii  
-wget -i images.txt  # pobiera do obecnego folderu
+wget -i images.txt -U 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36' # pobiera do obecnego folderu
 ```
--- naszemiasto.py
+++ naszemiasto.py
@@ -1,8 +1,10 @@
 import requests
 import sys
 from bs4 import BeautifulSoup
+import re
 
 url = sys.argv[1]
+header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
 
 domain = '/'.join(url.split("/")[:3])  # https + domain name
 n = 0
@@ -16,17 +18,17 @@ try:
 	while True:
 		n += 1
 		print(f"[*] Fetching page {n}... ", end='')
-		r = requests.get(url)
+		r = requests.get(url,headers=header)
 		print("fetched... ", end='')
 		soup = BeautifulSoup(r.text, "html.parser")
-		a = soup.find("a", {"id": "material-galeria-nastepne"})
-		img = soup.find("figure", {"class": "galZdjecie"}).find("img")
+		a = soup.find("a", {"class": "atomsGalleryButtonsNavigation__right"})
+		img = soup.find("source", {"srcset" : re.compile("xlarge\.jpg")})
 		if not img:
 			print("[!] Image not found! Exiting...")
 			break
 
 		print("image found!")
-		images.append(img['src'].split('?')[0])
+		images.append(img['srcset'].split('?')[0])
 		if not a:
 			break
 
